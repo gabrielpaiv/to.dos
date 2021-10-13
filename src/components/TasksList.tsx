c536dd89164a7ae8d16d6865506449a4b7d63a92
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-10-13 11:55:53 -0300

    Finished TasksLists Finished Home Challenge Completed!

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,27 +1,39 @@
-import React from 'react';
-import { FlatList, Image, TouchableOpacity, View, Text, StyleSheet, FlatListProps } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
+import React from 'react'
+import {
+  FlatList,
+  Image,
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  FlatListProps
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
 
-import { ItemWrapper } from './ItemWrapper';
+import { ItemWrapper } from './ItemWrapper'
 
 import trashIcon from '../assets/icons/trash/trash.png'
 
 export interface Task {
-  id: number;
-  title: string;
-  done: boolean;
+  id: number
+  title: string
+  done: boolean
 }
 
 interface TasksListProps {
-  tasks: Task[];
-  toggleTaskDone: (id: number) => void;
-  removeTask: (id: number) => void;
+  tasks: Task[]
+  toggleTaskDone: (id: number) => void
+  removeTask: (id: number) => void
 }
 
-export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps) {
+export function TasksList({
+  tasks,
+  toggleTaskDone,
+  removeTask
+}: TasksListProps) {
   return (
     <FlatList
-      // data={tasks}
+      data={tasks}
       keyExtractor={item => String(item.id)}
       contentContainerStyle={{ paddingBottom: 24 }}
       showsVerticalScrollIndicator={false}
@@ -33,23 +45,20 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
                 testID={`button-${index}`}
                 activeOpacity={0.7}
                 style={styles.taskButton}
-                //TODO - use onPress (toggle task) prop
+                // TODO - use onPress (toggle task) prop
+                onPress={() => toggleTaskDone(item.id)}
               >
-                <View 
+                <View
                   testID={`marker-${index}`}
-                  //TODO - use style prop 
+                  //TODO - use style prop
+                  style={item.done ? styles.taskMarkerDone : styles.taskMarker}
                 >
-                  { item.done && (
-                    <Icon 
-                      name="check"
-                      size={12}
-                      color="#FFF"
-                    />
-                  )}
+                  {item.done && <Icon name="check" size={12} color="#FFF" />}
                 </View>
 
-                <Text 
+                <Text
                   //TODO - use style prop
+                  style={item.done ? styles.taskTextDone : styles.taskText}
                 >
                   {item.title}
                 </Text>
@@ -60,6 +69,7 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
               testID={`trash-${index}`}
               style={{ paddingHorizontal: 24 }}
               //TODO - use onPress (remove task) prop
+              onPress={() => removeTask(item.id)}
             >
               <Image source={trashIcon} />
             </TouchableOpacity>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from '../components/Header'
+import { Task, TasksList } from '../components/TasksList'
+import { TodoInput } from '../components/TodoInput'
 
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([])
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: Number(new Date().getTime()),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = [...tasks]
+    const taskDone = updatedTasks.find(item => item.id === id)
+    if (taskDone) {
+      taskDone.done = !taskDone.done
+      setTasks(updatedTasks)
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id))
   }
 
   return (
@@ -26,10 +36,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
